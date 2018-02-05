52c2bc5a797b6070a32acfad5824f17d4a7d6e62
alee-altiscale <alee@altiscale.com>
2018-02-05 12:12:57 -0800

    Remove mock and rpm spec files, and revert to the hacky way to generate rpm via fpm.

--- scripts/justbuild.sh
+++ scripts/justbuild.sh
@@ -4,18 +4,9 @@
 
 curr_dir=`dirname $0`
 curr_dir=`cd $curr_dir; pwd`
-workspace_dir=$curr_dir
-workspace_rpm_dir=$workspace_dir/workspace_rpm
-WORKSPACE=${WORKSPACE:-$workspace_rpm_dir}
-mkdir -p $workspace_rpm_dir
-livy_git_dir=$workspace_dir/../incubator-livy
-livy_spec="$curr_dir/livy.spec"
+mkdir -p $WORKSPACE
+livy_git_dir=$WORKSPACE/incubator-livy
 git_hash=""
-mvn_settings="$HOME/.m2/settings.xml"
-mvn_runtime_settings="$curr_dir/settings.xml"
-mvn_macros_def_list=
-additional_mvn_build_args=
-builddir_mvn_settings="/tmp/settings.xml"
 
 if [ -f "$curr_dir/setup_env.sh" ]; then
   set -a
@@ -39,7 +30,7 @@ popd
 # Get a copy of the source code, and tar ball it, remove .git related files
 # Rename directory from livy to alti-livy to distinguish 'livy' just in case.
 echo "ok - tar zip livy-xxx source file, preparing for build/compile by rpmbuild"
-pushd $workspace_rpm_dir
+pushd $WORKSPACE
 pushd $livy_git_dir/../
   tar --exclude .git --exclude .gitignore -cf $workspace_rpm_dir/livy.tar incubator-livy
 popd
@@ -53,55 +44,48 @@ mv incubator-livy alti-livy
 tar --exclude .git --exclude .gitignore -czf alti-livy.tar.gz alti-livy
 popd
 
-# Launch mock to build Altiscale Spark
-pushd $workspace_rpm_dir
-rm -rf *.rpm
-echo "ok - producing $LIVY_PKG_NAME spec file"
-cp $livy_spec .
-spec_name=$(basename $livy_spec)
-echo "ok - applying version number $LIVY_VERSION and other env variables to $(pwd)/$spec_name via rpm macros"
+echo "build - entire livy project in $workspace_rpm_dir"
 
-if [ -f "$mvn_settings" ] ; then
-  diff -q $mvn_settings $mvn_runtime_settings
-  if [ $? -eq "0" ] ; then
-    echo "ok - $mvn_settings content is the same as local copy, apply local copy due to permission tweak 644"
-    mvn_macros_def_list="_mvn_settings $builddir_mvn_settings"
-    additional_mvn_build_args="--copyin=$mvn_runtime_settings:$builddir_mvn_settings"
-  else
-    echo "ok - $mvn_settings content is different from the local copy, use $mvn_settings for safety"
-    mvn_macros_def_list="_mvn_settings $builddir_mvn_settings"
-    additional_mvn_build_args="--copyin=$mvn_settings:$builddir_mvn_settings"
-  fi
-
-  alti_mock build --root=$BUILD_ROOT \
-    --spec=./$spec_name \
-    -S ./alti-livy.tar.gz \
-    -D "_current_workspace $WORKSPACE"\
-    "_livy_version $LIVY_VERSION" "_scala_build_version $SCALA_VERSION" "_git_hash_release $git_hash"\
-    "_hadoop_version $HADOOP_VERSION" "_spark_version $SPARK_VERSION" "_altiscale_release_ver $ALTISCALE_RELEASE"\
-    "_apache_name $LIVY_PKG_NAME"\
-    "_build_release $BUILD_TIME" "_production_release $PRODUCTION_RELEASE"\
-    "$mvn_macros_def_list"\
-    "$additional_mvn_build_args"
-else
-  2>&1 echo "warn - $mvn_settings not found, env is incorrect and may expose to public repo directly!!!!!"
-  alti_mock build --root=$BUILD_ROOT \
-    --spec=./$spec_name \
-    -S ./alti-livy.tar.gz \
-    -D "_current_workspace $WORKSPACE"\
-    "_livy_version $LIVY_VERSION" "_scala_build_version $SCALA_VERSION" "_git_hash_release $git_hash"\
-    "_hadoop_version $HADOOP_VERSION" "_spark_version $SPARK_VERSION" "_altiscale_release_ver $ALTISCALE_RELEASE"\
-    "_apache_name $LIVY_PKG_NAME"\
-    "_build_release $BUILD_TIME" "_production_release $PRODUCTION_RELEASE"
+if [ "x${HADOOP_VERSION}" = "x" ] ; then
+  echo "fatal - HADOOP_VERSION needs to be set, can't build anything, exiting"
+  exit -8
 fi
 
+echo "ok - building entire pkg with HADOOP_VERSION=$HADOOP_VERSION SPARK_VERSION=$SPARK_VERSION scala=$SCALA_VERSION"
+
+# PURGE LOCAL CACHE for clean build
+# mvn dependency:purge-local-repository
+
+########################
+# BUILD ENTIRE PACKAGE #
+########################
+# Default JDK version applied is 1.8 here.
+
+spark_profile_str=""
+if [[ $SPARK_VERSION == 1.* ]] ; then
+  spark_profile_str="-Pspark-1.6"
+elif [[ $SPARK_VERSION == 2.1.* ]] ; then
+  spark_profile_str="-Pspark-2.1"
+elif [[ $SPARK_VERSION == 2.2.* ]] ; then
+  spark_profile_str="-Pspark-2.2"
+else
+  echo "fatal - Unrecognize spark version $SPARK_VERSION, can't continue, exiting, no cleanup"
+  exit -9
+fi
+
+mvn_cmd="mvn -U -X $spark_profile_str package"
+echo "$mvn_cmd"
+$mvn_cmd
+
 if [ $? -ne "0" ] ; then
-  echo "fail - $spec_name SRPM build failed"
+  echo "fail - Livy $LIVY_VERSION build failed!"
   popd
   exit -99
 fi
 popd
 
-echo "ok - build Completed successfully!"
+
+
+echo "ok - build Livy $LIVY_VERSION completed successfully!"
 
 exit 0
--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -3,12 +3,14 @@
 # TBD: honor system pre-defined property/variable files from 
 # /etc/hadoop/ and other /etc config for spark, hdfs, hadoop, etc
 
-# Force to use default Java which is JDK 1.7 now
+
+curr_dir=`dirname $0`
+curr_dir=`cd $curr_dir; pwd`
+
 export JAVA_HOME=${JAVA_HOME:-"/usr/java/default"}
 export ANT_HOME=${ANT_HOME:-"/opt/apache-ant"}
 export MAVEN_HOME=${MAVEN_HOME:-"/usr/share/apache-maven"}
 export M2_HOME=${M2_HOME:-"/usr/share/apache-maven"}
-export MAVEN_OPTS=${MAVEN_OPTS:-"-Xmx2g -XX:MaxPermSize=1024M -XX:ReservedCodeCacheSize=512m"}
 export SCALA_HOME=${SCALA_HOME:-"/opt/scala"}
 export HADOOP_VERSION=${HADOOP_VERSION:-"2.7.4"}
 # Spark 1.5+ default Hive starts with 1.2.1, backward compatible with Hive 1.2.0
@@ -16,37 +18,16 @@ export SPARK_VERSION=${SPARK_VERSION:-"2.2.1"}
 
 export PATH=$PATH:$M2_HOME/bin:$SCALA_HOME/bin:$ANT_HOME/bin:$JAVA_HOME/bin
 
-# Define default spark uid:gid and build version
-# and all other Spark build related env
-export LIVY_PKG_NAME=${LIVY_PKG_NAME:-"livy"}
+# Honor incubator-livy-pkg-env.sh env varibales and value
+export PACKAGE_NAME=${PACKAGES:-"incubator-livy"}
 export LIVY_VERSION=${LIVY_VERSION:-"0.4.0"}
+export MAVEN_OPTS=${MAVEN_OPTS:-"-Xmx2048m -XX:MaxPermSize=1024m"}
 
-# Defines which Hadoop version to build against. Always use the latest as default.
-export ALTISCALE_RELEASE=${ALTISCALE_RELEASE:-"4.3.0"}
-if [[ $HADOOP_VERSION == 2.2.* ]] ; then
-  TARGET_ALTISCALE_RELEASE=2.0.0
-elif [[ $HADOOP_VERSION == 2.4.* ]] ; then
-  TARGET_ALTISCALE_RELEASE=3.0.0
-elif [[ $HADOOP_VERSION == 2.[67].* ]] ; then
-  TARGET_ALTISCALE_RELEASE=4.3.0
-else
-  2>&1 echo "error - can't recognize altiscale's HADOOP_VERSION=$HADOOP_VERSION for $ALTISCALE_RELEASE"
-  2>&1 echo "error - $LIVY_VERSION has not yet been tested nor endorsed by Altiscale on $HADOOP_VERSION"
-  2>&1 echo "error - We won't continue to build Spark $LIVY_VERSION, exiting!"
-  exit -1
-fi
-# Sanity check on RPM label integration and Altiscale release label
-if [ $TARGET_ALTISCALE_RELEASE != $ALTISCALE_RELEASE ] ; then
-  2>&1 echo "fatal - you specified $ALTISCALE_RELEASE that is not verified by $LIVY_VERSION yet"
-  2>&1 echo "fatal - releasing this will potentially break Spark installaion or Hadoop compatibility, exiting!"
-  exit -2
-fi
-
-export BUILD_TIMEOUT=${BUILD_TIMEOUT:-"86400"}
 # centos6.5-x86_64
 # centos6.6-x86_64
 # centos6.7-x86_64
-export BUILD_ROOT=${BUILD_ROOT:-"centos6.5-x86_64"}
 export BUILD_TIME=$(date +%Y%m%d%H%M)
 # Customize build OPTS for MVN
-export MAVEN_OPTS=${MAVEN_OPTS:-"-Xmx2048m -XX:MaxPermSize=1024m"}
+
+workspace_dir=$curr_dir
+export WORKSPACE=${WORKSPACE:-$workspace_dir}
